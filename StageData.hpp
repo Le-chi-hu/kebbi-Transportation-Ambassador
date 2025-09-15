054e8e4977ff0baf6e9e3a70af30ae1fbc04aa8f
10969Ethan <167860998+10969Ethan@users.noreply.github.com>
2025-09-15 16:46:20 +0800

    Update StageData.hpp

--- StageData.hpp
+++ StageData.hpp
@@ -4,10 +4,8 @@
 #include <string>
 #include <chrono>
 
-// 使用 'using namespace std::chrono_literals;' 讓 's' 字尾可用
 using namespace std::chrono_literals;
 
-// 定義一個結構來儲存每個教學階段的資訊
 struct Stage {
     std::string m_strStageName;
     std::string m_strSystemMessage;
@@ -15,5 +13,5 @@ struct Stage {
     std::chrono::seconds m_secDurationLimit;
 };
 
-// 宣告一個函式，它會回傳所有教學階段的向量
+// 宣告一個函式，回傳所有教學階段的向量
 std::vector<Stage> GetAllStages();
